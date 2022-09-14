bccdfa9c76340921df899d93a02487ac5c326422
Babbs-UX <tyler.r.uxd@gmail.com>
2022-09-14 18:54:50 -0400

    swap again

--- test.php
+++ test.php
@@ -1,9 +1,16 @@
 <?php
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-echo 'Map Saved!'
+    if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
+    {
+        func();
+    }
+    function func()
+    {
+        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        $txt = "John Doe\n";
+        fwrite($myfile, $txt);
+        $txt = "Jane Doe\n";
+        fwrite($myfile, $txt);
+        fclose($myfile);
+        echo 'Map Saved!'   
+            }
 ?>
